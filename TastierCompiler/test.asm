6cc3599a76fd479cd8306be21b1ef7c6a593debf
mashikag <littlesoldiergraba@gmail.com>
2014-12-29 05:41:16 +0000

    fuxung

--- TastierCompiler/test.asm
+++ TastierCompiler/test.asm
@@ -36,33 +36,44 @@ L$3: Nop
 Sto 0 0
 Load 0 0
 Write
+Const 5
+Load 0 0
+Equ
+FJmp L$5
+Const 5
+Write
+Jmp L$4
+L$5: Nop
+Const 7
+Write
+L$4: Nop
 Const 10
 Sto 0 0
-L$4: Nop
+L$6: Nop
 Load 0 0
 Const 1
 Sub
 Load 0 0
 Const 0
 Gtr
-FJmp L$5
+FJmp L$7
 Load 0 0
 Write
 Sto 0 0
-Jmp L$4
-L$5: Nop
+Jmp L$6
+L$7: Nop
 Read
 StoG 3
-L$6: Nop
+L$8: Nop
 LoadG 3
 Const 0
 Gtr
-FJmp L$7
+FJmp L$9
 Call 1 SumUp
 Read
 StoG 3
-Jmp L$6
-L$7: Nop
+Jmp L$8
+L$9: Nop
 Const 5
 Write
 Leave
