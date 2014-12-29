dbee178e55c8c2af74f66ba50123cb695ccbd359
mashikag <littlesoldiergraba@gmail.com>
2014-12-29 05:54:48 +0000

    fuxung

--- TastierCompiler/test.asm
+++ TastierCompiler/test.asm
@@ -1,79 +1,29 @@
-.names 5
+.names 4
 .proc Main
-.proc SumUp
 .var 1 i
-SumUp: Enter 1
-Const 0
-Sto 0 0
-L$0: Nop
-LoadG 3
-Const 0
-Gtr
-FJmp L$1
-Load 0 0
-LoadG 3
-Add
-Sto 0 0
-LoadG 3
-Const 1
-Sub
-StoG 3
-Jmp L$0
-L$1: Nop
-Load 0 0
-Write
-Leave
-Ret
 Main: Enter 1
 Const 0
-FJmp L$2
+FJmp L$0
 Const 1
-Jmp L$3
-L$2: Nop
+Jmp L$1
+L$0: Nop
 Sto 0 0
 Const 2
-L$3: Nop
+L$1: Nop
 Sto 0 0
 Load 0 0
 Write
 Const 5
 Load 0 0
 Equ
-FJmp L$5
+FJmp L$3
 Const 5
 Write
-Jmp L$4
-L$5: Nop
+Jmp L$2
+L$3: Nop
 Const 7
 Write
-L$4: Nop
-Const 10
-Sto 0 0
-L$6: Nop
-Load 0 0
-Const 1
-Sub
-Load 0 0
-Const 0
-Gtr
-FJmp L$7
-Load 0 0
-Write
-Sto 0 0
-Jmp L$6
-L$7: Nop
-Read
-StoG 3
-L$8: Nop
-LoadG 3
-Const 0
-Gtr
-FJmp L$9
-Call 1 SumUp
-Read
-StoG 3
-Jmp L$8
-L$9: Nop
+L$2: Nop
 Const 5
 Write
 Leave
